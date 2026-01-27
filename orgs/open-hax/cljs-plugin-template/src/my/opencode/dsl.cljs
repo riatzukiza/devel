c38d37f67b8763e1db6d6d95305bafdca9a387ad
Error <foamy125@gmail.com>
2026-01-26 20:34:30 -0600

    Add ollama-benchmarks and promethean-agent-system as submodules

--- orgs/open-hax/cljs-plugin-template/src/my/opencode/dsl.cljs
+++
@@ -1,4 +0,0 @@
-(ns my.opencode.dsl
-  (:require-macros [my.opencode.dsl.macros :refer [defplugin hook before-tool after-tool tools extra init]])
-  (:require [my.opencode.spec]
-            [my.opencode.runtime]))
