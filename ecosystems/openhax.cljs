3f5e8351fbc78f54b2b923314d1c9aae1db06900
Error <foamy125@gmail.com>
2026-02-07 20:07:55 -0600

    updated ecosystems

--- ecosystems/openhax.cljs
+++ ecosystems/openhax.cljs
@@ -4,23 +4,23 @@
 (ns openhax
   (:require [clobber.macro]))
 
-(clobber.macro/defapp "open-hax-workbench"
-  {:script "bash"
-   :args ["-lc" "pnpm build:clojurescript && cp static/index.html public/index.html && python3 -m http.server 8080 -d public"]
-   :cwd "orgs/open-hax/workbench"
-   :interpreter "/usr/bin/env"
-   :exec_mode "fork"
-   :instances 1
-   :env {:NODE_ENV "development"}
-   :max_restarts 5
-   :restart_delay 2000
-   :autorestart true
-   :watch false
-   :error_file "./logs/open-hax-workbench-error.log"
-   :out_file "./logs/open-hax-workbench-out.log"
-   :log_file "./logs/open-hax-workbench.log"
-   :time true
-   :kill_timeout 5000})
+;; (clobber.macro/defapp "open-hax-workbench"
+;;   {:script "bash"
+;;    :args ["-lc" "pnpm build:clojurescript && cp static/index.html public/index.html && python3 -m http.server 8081 -d public"]
+;;    :cwd "orgs/open-hax/workbench"
+;;    :interpreter "/usr/bin/env"
+;;    :exec_mode "fork"
+;;    :instances 1
+;;    :env {:NODE_ENV "development"}
+;;    :max_restarts 5
+;;    :restart_delay 2000
+;;    :autorestart true
+;;    :watch false
+;;    :error_file "./logs/open-hax-workbench-error.log"
+;;    :out_file "./logs/open-hax-workbench-out.log"
+;;    :log_file "./logs/open-hax-workbench.log"
+;;    :time true
+;;    :kill_timeout 5000})
 (clobber.macro/defapp "open-hax-workbench-dev"
   {:script "bash"
    :args ["-lc" "pnpm dev"]
