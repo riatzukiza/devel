684fefe7197daf9d19395e4493976437eec27d57
Err <foamy125@gmail.com>
2026-03-30 10:28:54 -0500

    fix: add ESM extensions for event package exports

--- packages/event/src/index.ts
+++ packages/event/src/index.ts
@@ -1,6 +1,6 @@
-export * from './types';
-export * from './memory';
-export { Topics } from './topics';
-export * from './config';
-export * from './outbox';
-export * from './mongo';
+export * from './types.js';
+export * from './memory.js';
+export { Topics } from './topics.js';
+export * from './config.js';
+export * from './outbox.js';
+export * from './mongo.js';
